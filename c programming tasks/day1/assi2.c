4335df94802fbed7c9f97d25d5cffa5ca6d9e07c
abdulazizElmohandis <azizelmohandis@gmail.com>
2022-08-10 16:58:50 +0200

    added till day3

--- c programming tasks/day1/assi2.c
+++ c programming tasks/day1/assi2.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
-
+#include <string.h>
 void main(void){
 
-int a,b ,mul ,sum;
-	
-printf("     *     \n"
-"    ***     \n"
-"   *****     \n"
-"  *******     \n"
-" *********     \n"
+int x, reverse = 0, remaining;  
+
+  
+printf("\nEnter number:"); 
+   
+  scanf("%d",&x);   
+  
+  while(x != 0)    
+  {    
+     remaining = x % 10;    /*get last digit*/ 
+	 
+     reverse = (reverse * 10) + remaining;   
+     x /= 10;    /*remove last digit*/ 
+  }    
+  
+  
+  printf("\nReversed number: %d",reverse);   
 
 
-);
 
 }
